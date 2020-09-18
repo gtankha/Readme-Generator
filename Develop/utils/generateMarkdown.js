d290787059f14afba69379fdc5853f3fc6da3af4
gtankha <46304828+gtankha@users.noreply.github.com>
2020-09-17 23:09:20 -0700

    adjust readme.md

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -15,13 +15,13 @@ const generateMarkdown = data => {
 
   ## Table of Contents
 
-  ## [1. Description] (#description)
-  ## [2. Installation] (#installation)
-  ## [3. Usage] (#usage)
-  ## [4. License] (#license)
-  ## [5. Contributing] (#contributing)
-  ## [6. Tests] (#tests)
-  ## [7. Questions] (#questions)
+  1. [Description] (#description)
+  2. [Installation] (#installation)
+  3. [Usage] (#usage)
+  4. [License] (#license)
+  5. [Contributing] (#contributing)
+  6. [Tests] (#tests)
+  7. [Questions] (#questions)
 
 
   <a name="installation"></a>
